786fdf6d55e56d5c2539bd7bd6e81c8ccdee83dd
timecorner35 <71405627+timecorner35@users.noreply.github.com>
2020-10-23 22:08:59 +0100

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -7,35 +7,23 @@ public class Q1 {
 		System.out.println("enter your first number");
 		Scanner scan = new Scanner(System.in);
 		int num1 = scan.nextInt();
+		int smallest, largest;
+		smallest = num1;
+		largest = num1;
 		
+		while(true) {
 		System.out.println("enter your next number");
 		int num2 = scan.nextInt();
-		int smallest, largest;
-		if (num1<=num2) {smallest = num1; largest = num2;}
-		else {smallest = num2; largest = num1;}
+		if (num2>largest)largest = num2;
+		if(num2<smallest) smallest = num2;
+		
 		System.out.println("do you wanna enter another one?		0:No	1:Yes");
 		int answer = scan.nextInt();
-		if (answer==1)
-		do{
+		if (answer==1) continue;
+		else break;
+		}
 		
 			
-			System.out.println("enter your next number");
-			int inp1 = scan.nextInt();
-			if(inp1<smallest) {
-				smallest = inp1;
-			}
-			else if(inp1>largest) {
-				largest = inp1;
-			}
-			
-			System.out.println("do you wanna enter another one?		0:No	1:Yes");
-			int secanswer = scan.nextInt();
-			if (secanswer==0) {
-				System.out.println("smallest number is :"+smallest+"\nlargest number is: "+largest);
-				return;
-				}
-			
-		}while(answer==1);
 		
 		System.out.println("smallest number is :"+smallest+"\nlargest number is: "+largest);
 		
